25f5c99b94b1d85fafae9a7c9bc00fd6f8881fd5
ajinkyapote1 <ajinkyapote1@gmail.com>
2018-12-17 00:43:20 +0530

    adding retrofit module

---
+++ retrofit/src/main/java/com/example/retrofit/network/APIInterface.java
@@ -0,0 +1,6 @@
+package com.example.retrofit.network;
+
+public interface APIInterface {
+
+
+}
---
+++ retrofit/src/main/java/com/example/retrofit/network/RetrofitClient.java
@@ -0,0 +1,24 @@
+package com.example.retrofit.network;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "";
+
+    public static Retrofit getRetroftClient() {
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+
+        return retrofit;
+    }
+}
